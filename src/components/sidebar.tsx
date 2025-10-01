91d1d37a7fa4891dcb5850262277a44b57595422
HeesooJun <125811119+HeesooJun@users.noreply.github.com>
2025-10-02 00:37:30 +0900

    feat: sidebar UI skeleton add & pretendard impl (#9)
    * feat: sidebar UI skeleton add & pretendard impl
    
    * logo svg로 변환 및 tailwind 충돌 방지
    
    * 구분선 w-full로 변환
    
    * conflict
    
    * Delete tailwind.config.js

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,28 +1,39 @@
 // src/components/Sidebar.tsx
-import { NavLink, Outlet } from 'react-router-dom'
-import '../styles/sidebar.css'
+import { NavLink, Outlet } from "react-router-dom";
+import "@/styles/sidebar.css";
+import logo from "@/assets/logo.svg";
+
+// NavLink
+const getNavLink = ({ isActive }: { isActive: boolean }) =>
+  isActive ? "layout_link is-active" : "layout_link";
 
 export default function Sidebar() {
   return (
     <div className="layout">
-      <aside className="layout__sidebar">
-        <h2>Sidebar</h2>
+      <aside className="layout_sidebar">
+        <div className="layout_brand">
+          <img src={logo} alt="logo" className="layout_brandIcon" />
+          <div className="layout_brandText">Performance Test</div>
+        </div>
+        <div className="layout_divider"></div>
         <nav>
           <ul>
             <li>
-              <NavLink to="dashboard" end>
-                Dashboard
+              <NavLink to="dashboard" className={getNavLink} end>
+                대시보드
               </NavLink>
             </li>
             <li>
-              <NavLink to="impr" end>Impr</NavLink>
+              <NavLink to="impr" className={getNavLink} end>
+                개선방안 & 효과
+              </NavLink>
             </li>
           </ul>
         </nav>
       </aside>
-      <section className="layout__content">
+      <section className="layout_content">
         <Outlet />
       </section>
     </div>
-  )
+  );
 }
