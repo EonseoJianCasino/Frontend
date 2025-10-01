3ccef6433909b1977ed012e82e2302c49ff9d77a
Heesoo Jun <acesk123@gmail.com>
2025-10-01 23:37:26 +0900

    fix: fileURLToPath add

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.config";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
