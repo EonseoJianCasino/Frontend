05d32fd0308073d3d8d70424855b99faccf11a0d
a <acesk123@gmail.com>
2025-10-01 17:55:10 +0900

    feat: sidebar UI skeleton add & pretendard impl

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,28 +1,46 @@
 // src/components/Sidebar.tsx
-import { NavLink, Outlet } from 'react-router-dom'
-import '../styles/sidebar.css'
+import { NavLink, Outlet } from "react-router-dom";
+import "../styles/sidebar.css";
 
 export default function Sidebar() {
   return (
     <div className="layout">
-      <aside className="layout__sidebar">
-        <h2>Sidebar</h2>
+      <aside className="layout_sidebar">
+        <div className="layout_brand">
+          <img
+            src="https://api.builder.io/api/v1/image/assets/TEMP/c23528196faf0c43a2f9444740f85535c37967c1?placeholderIfAbsent=true"
+            alt="Performance icon"
+            className="layout_brandIcon"
+          />
+          <div className="layout_brandText">Performance Test</div>
+        </div>
+        <div className="layout_divider"></div>
         <nav>
           <ul>
             <li>
-              <NavLink to="dashboard" end>
-                Dashboard
+              <NavLink
+                to="dashboard"
+                className={({ isActive }) => (isActive ? "layout_link is-active" : "layout_link")}
+                end
+              >
+                대시보드
               </NavLink>
             </li>
             <li>
-              <NavLink to="impr" end>Impr</NavLink>
+              <NavLink
+                to="impr"
+                className={({ isActive }) => (isActive ? "layout_link is-active" : "layout_link")}
+                end
+              >
+                개선방안 & 효과
+              </NavLink>
             </li>
           </ul>
         </nav>
       </aside>
-      <section className="layout__content">
+      <section className="layout_content">
         <Outlet />
       </section>
     </div>
-  )
+  );
 }
