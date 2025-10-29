ced0aedce2a4f2f7c146dd15bd167f2cc40154bc
Jiny <moc24@naver.com>
2025-10-30 01:20:42 +0900

    feat : 개선방안/효과 페이지 타이틀, 상단 도넛 부분(점수) 제작

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Sidebar from './components/sidebar'
 import Dashboard from './pages/Dashboard/Dashboard'
-import Impr from './pages/Impr'
 import Popup from './pages/Popup'
+import SolutionPage from './pages/SolutionPage/SolutionPage'
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
       <Route path="app" element={<Sidebar />}>
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="impr" element={<Impr />} />
+        <Route path="impr" element={<SolutionPage />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />
