a1f0eddd5e822b6133c21648505e79bb62f15b0e
Jiny <moc24@naver.com>
2025-10-30 23:34:59 +0900

    feat : 단계별 개선 방안 간단하게 틀 제작

--- src/pages/SolutionPage/SolutionPage.tsx
+++ src/pages/SolutionPage/SolutionPage.tsx
@@ -1,4 +1,5 @@
 import ArrowRight from '../../assets/icons/arrowRight.svg'
+import styles from './SolutionPage.module.scss'
 export default function SolutionPage() {
   return (
     // TODO : 차트 동적으로 되게, 더 동글동글하게 ui 바꿔야함
@@ -11,7 +12,7 @@ export default function SolutionPage() {
       </header>
 
       {/* 상단 메인 카드 */}
-      <section className="box-border flex w-full flex-col items-center">
+      <section className="box-border flex w-full flex-col items-center gap-y-6">
         <article className="relative box-border flex h-[213px] w-full max-w-[550px] flex-row items-center justify-around rounded-[15px] border-2 border-solid border-[#DEEBEF] bg-[#FFFFFF] p-4 shadow-md">
           <section className="relative flex flex-col items-center gap-y-3">
             <h3 className="absolute -top-7 text-[14px] text-[#4B4B4B]">기존 점수</h3>
@@ -31,6 +32,38 @@ export default function SolutionPage() {
             <span className="absolute -bottom-8 text-[16px] font-semibold text-[#3A7CA5]">
               24점 +
             </span>
+          </section>
+        </article>
+
+        {/* 단계별 개선 방안  */}
+        <article className={styles.step_solution_container}>
+          <h2 className={styles.title}>단계별 개선 방안</h2>
+
+          <section className={styles.sub_container}>
+            <div>
+              <span>긴급</span>
+            </div>
+            <h3 className={styles.sub_title}>CLS(레이아웃 이동) 최적화</h3>
+            <p className={styles.sub_summary}>
+              이미지와 광고 영역의 크기를 미리 지정하여 레이아웃 이동을 방지합니다. 현재 0.25에서
+              0.08로 개선 가능합니다.
+            </p>
+
+            <div>
+              <div>
+                <h4>예상 개선 효과</h4>
+                <div>성능 점수 +15점</div>
+              </div>
+              <div>
+                <h4>연관 지표</h4>
+                <div>
+                  <div>LCP</div>
+                  <div>CLS</div>
+                </div>
+              </div>
+            </div>
+
+            <div>자세히 보기 {'>'}</div>
           </section>
         </article>
       </section>
