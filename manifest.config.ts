17efe989fdd9a10656da85bd12e47c93fd5e4ede
a <acesk123@gmail.com>
2025-11-13 17:29:57 +0900

    build: .env 주입 및 host_permission 설정

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,11 +1,16 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
-export default defineManifest(() => ({
-  manifest_version: 3,
-  name: 'Chrome Extension Performance Test',
-  version: '1.0.0',
-  action: {
-    default_popup: 'index.html#/popup',
-  },
-  permissions: ['tabs'],
-}))
+export default defineManifest(() => {
+  const hp = process.env.VITE_HOST_PERMISSIONS!
+
+  return {
+    manifest_version: 3,
+    name: 'Chrome Extension Performance Test',
+    version: '1.0.0',
+    action: {
+      default_popup: 'index.html#/popup',
+    },
+    permissions: ['tabs'],
+    host_permissions: [hp],
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
-  plugins: [react(), crx({ manifest })],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  Object.assign(process.env, env)
+
+  return {
+    plugins: [react(), crx({ manifest })],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    cors: {
-      origin: [/chrome-extension:\/\//],
+    server: {
+      cors: {
+        origin: [/chrome-extension:\/\//],
+      },
     },
-  },
+  }
 })
