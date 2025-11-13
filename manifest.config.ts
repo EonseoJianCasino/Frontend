f56ed1b95adb6dbf03327e05ccf69a84b4834ea7
a <acesk123@gmail.com>
2025-11-13 17:36:42 +0900

    build: host_permission name changed and catch error

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,10 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
 export default defineManifest(() => {
-  const hp = process.env.VITE_HOST_PERMISSIONS!
+  const hostPermission = process.env.VITE_HOST_PERMISSIONS
+  if (!hostPermission) {
+    throw new Error('VITE_HOST_PERMISSIONS is required.')
+  }
 
   return {
     manifest_version: 3,
@@ -11,6 +14,6 @@ export default defineManifest(() => {
       default_popup: 'index.html#/popup',
     },
     permissions: ['tabs'],
-    host_permissions: [hp],
+    host_permissions: [hostPermission],
   }
 })
