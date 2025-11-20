20a3d9063ec925ce9983e37100d107b2bad72cd8
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-11-20 16:47:43 +0900

    feat : 개선 기대효과 슬라이더 제작 (#24)

--- src/pages/SolutionPage/SolutionPage.tsx
+++ src/pages/SolutionPage/SolutionPage.tsx
@@ -1,5 +1,74 @@
 import ArrowRight from '../../assets/icons/arrowRight.svg'
 import styles from './SolutionPage.module.scss'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import lightningImg from '../../assets/icons/lightning.svg'
+// * 개선방안 슬라이더
+const SolutionSlider: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  }
+  return (
+    <div className="slider-container mx-[1%] h-[230px] w-full">
+      <Slider {...settings}>
+        {/* 
+        // TODO 추후 article 부분 소프트 코딩으로 변경 
+        
+        */}
+        <article className="box-border flex h-[225px] max-w-[95%] flex-1 flex-col rounded-[15px] bg-white p-10 shadow-md">
+          <div className="box-border flex h-[33px] w-[33px] items-center justify-center rounded-full bg-[#3B82F6]">
+            <img src={lightningImg} alt="번개" />
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#4B4B4B]">
+            <span className="text-[#3B82F6]">LCP</span> 0.8초 단축
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#707071]">
+            첫 화면 로딩이 빨라집니다.
+          </div>
+        </article>
+        <article className="box-border flex h-[225px] max-w-[95%] flex-1 flex-col rounded-[15px] bg-white p-10 shadow-md">
+          <div className="box-border flex h-[33px] w-[33px] items-center justify-center rounded-full bg-[#3B82F6]">
+            <img src={lightningImg} alt="번개" />
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#4B4B4B]">
+            <span className="text-[#3B82F6]">LCP</span> 0.8초 단축
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#707071]">
+            첫 화면 로딩이 빨라집니다.
+          </div>
+        </article>
+        <article className="box-border flex h-[225px] max-w-[95%] flex-1 flex-col rounded-[15px] bg-white p-10 shadow-md">
+          <div className="box-border flex h-[33px] w-[33px] items-center justify-center rounded-full bg-[#3B82F6]">
+            <img src={lightningImg} alt="번개" />
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#4B4B4B]">
+            <span className="text-[#3B82F6]">LCP</span> 0.8초 단축
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#707071]">
+            첫 화면 로딩이 빨라집니다.
+          </div>
+        </article>
+        <article className="box-border flex h-[225px] max-w-[95%] flex-1 flex-col rounded-[15px] bg-white p-10 shadow-md">
+          <div className="box-border flex h-[33px] w-[33px] items-center justify-center rounded-full bg-[#3B82F6]">
+            <img src={lightningImg} alt="번개" />
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#4B4B4B]">
+            <span className="text-[#3B82F6]">LCP</span> 0.8초 단축
+          </div>
+          <div className="mt-2 pl-3 text-[21px] font-bold text-[#707071]">
+            첫 화면 로딩이 빨라집니다.
+          </div>
+        </article>
+      </Slider>
+    </div>
+  )
+}
+
 export default function SolutionPage() {
   return (
     // TODO : 차트 동적으로 되게, 더 동글동글하게 ui 바꿔야함
@@ -69,6 +138,12 @@ export default function SolutionPage() {
             </section>
             <div className={styles.btn_detail}>자세히 보기 {'>'}</div>
           </section>
+        </article>
+
+        {/* 개선 기대효과 */}
+        <article className={styles.solution_container}>
+          <h2 className={styles.title}>개선 기대효과</h2>
+          <SolutionSlider />
         </article>
       </section>
     </main>
