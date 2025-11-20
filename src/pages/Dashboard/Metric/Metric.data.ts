1a0be913801d69302d9d11dd42f1b6d98eb83dc5
Choi_ji_an <96537605+Ji-ny@users.noreply.github.com>
2025-11-20 16:47:16 +0900

    feat & fix : 대시보드 성능 지표, 보안지표 추가 및 기타 UI 수정 (#25)
    * feat: 성능 지표 & 보안 지표 컴포넌트 만들기 (스타일링 전)
    
    * feat: 성능 지표 & 보안 지표 스타일링
    
    * fix : 상태값 변경(상단), 성능 지표 글자 ui 수정, 제일 상단 블럭 sequrity 추가

---
+++ src/pages/Dashboard/Metric/Metric.data.ts
@@ -0,0 +1,74 @@
+import type { MetricItem } from './Metric.types'
+
+// 예시 더미 데이터 (성능 지표)
+export const performanceMetrics: MetricItem[] = [
+  {
+    metric: 'LCP (Largest Contentful Paint)',
+    message: '가장 큰 콘텐츠 로딩 시간',
+    value: '2.1초',
+    urgentStatus: 'GOOD',
+  },
+  {
+    metric: 'INP (Interaction to Next Paint)',
+    message: '상호작용 응답 시간',
+    value: '180ms',
+    urgentStatus: 'WARNING',
+  },
+  {
+    metric: 'CLS (Cumulative Layout Shift)',
+    message: '레이아웃 이동 정도',
+    value: '0.25',
+    urgentStatus: 'POOR',
+  },
+  {
+    metric: 'CLS (Cumulative Layout Shift)',
+    message: '레이아웃 이동 정도',
+    value: '0.25',
+    urgentStatus: 'POOR',
+  },
+  {
+    metric: 'CLS (Cumulative Layout Shift)',
+    message: '레이아웃 이동 정도',
+    value: '0.25',
+    urgentStatus: 'POOR',
+  },
+]
+
+// 예시 더미 데이터 (보안 지표)
+export const securityMetrics: MetricItem[] = [
+  {
+    metric: 'SSL',
+    message: 'SSL 유효 (61일 남음)',
+    urgentStatus: 'WARNING',
+  },
+  {
+    metric: 'HSTS',
+    message: 'HSTS 적용 (서브도메인 미포함)',
+    urgentStatus: 'WARNING',
+  },
+  {
+    metric: 'XCTO',
+    message: 'nosniff 적용됨',
+    urgentStatus: 'GOOD',
+  },
+  {
+    metric: 'REFERRER-POLICY',
+    message: 'Referrer-Policy 미설정',
+    urgentStatus: 'POOR',
+  },
+  {
+    metric: 'COOKIES',
+    message: '쿠키 보안 속성 양호 (Secure+HttpOnly+SameSite=Lax)',
+    urgentStatus: 'GOOD',
+  },
+  {
+    metric: 'CSP',
+    message: '안전한 CSP 적용',
+    urgentStatus: 'GOOD',
+  },
+  {
+    metric: 'FRAME-ANCESTORS/XFO',
+    message: 'X-Frame-Options 적용 (SAMEORIGIN)',
+    urgentStatus: 'GOOD',
+  },
+]
---
+++ src/pages/Dashboard/Metric/MetricCard.tsx
@@ -0,0 +1,40 @@
+import img_error from '@/assets/icons/error.svg'
+import img_ok from '@/assets/icons/ok.svg'
+import img_warning from '@/assets/icons/warning.svg'
+import type { MetricItem } from './Metric.types'
+
+/**
+ * 성능 지표, 보안 지표 카드
+ * @returns
+ */
+export default function MetricCard({ metric, message, value, urgentStatus }: MetricItem) {
+  console.log(metric, urgentStatus)
+  return (
+    <article className="box-border flex w-full min-w-[400px] flex-row items-center justify-between rounded-[10px] border-[1px] border-solid border-[#EBEEEE] bg-[#F8F9FA] p-3 px-6">
+      <div>
+        <h3 className="text-[16px] font-semibold text-[#4B4B4B]">{metric}</h3>
+        <label className="text-[12px] font-semibold text-[#888888]">{message}</label>
+      </div>
+      <div className="box-border flex flex-row items-center gap-x-3">
+        {value && (
+          <span
+            className={`text-[16px] font-semibold ${
+              urgentStatus === 'GOOD'
+                ? 'text-[#357BFA]'
+                : urgentStatus === 'WARNING'
+                  ? 'text-[#FABF35]'
+                  : 'text-[#FF3C3C]'
+            }`}
+          >
+            {value}{' '}
+          </span>
+        )}
+        <img
+          src={
+            urgentStatus === 'GOOD' ? img_ok : urgentStatus === 'WARNING' ? img_warning : img_error
+          }
+        ></img>
+      </div>
+    </article>
+  )
+}
